761c8188c4997c419e8d5be6db87f5cf1faa3884
Valera <kulkowalery@yandex.ru>
2017-11-14 22:32:47 +0200

    Composite pattern Tree structure creation

--- src/com/lesson_9/composite/Waitress.java
+++ src/com/lesson_9/composite/Waitress.java
@@ -14,4 +14,17 @@ public class Waitress {
     public void printMenu() {
         allMenus.print();
     }
+
+    public void printVegetarianMenu() {
+        Iterator<MenuComponent> iterator = allMenus.createIterator();
+        System.out.println("\nVEGETARIAN\n---------------------");
+        while (iterator.hasNext()) {
+            MenuComponent menuComponent = iterator.next();
+            try {
+                if (menuComponent.isVegetarian()) {
+                    menuComponent.print();
+                }
+            } catch (UnsupportedOperationException ignored) {}
+        }
+    }
 }
